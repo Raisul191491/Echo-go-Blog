6299e7d1674f87e26ee51b3864b5ccaebe11656d
Raisul191491 <aupiaupu@gmail.com>
2023-03-01 10:14:38 +0600

    User side complete

--- pkg/services/user-services.go
+++ pkg/services/user-services.go
@@ -3,9 +3,18 @@ package services
 import (
 	"errors"
 	"go-blog/pkg/models"
+	"go-blog/pkg/types"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 func RegisterUser(user *models.User) error {
+	tempPassword, err := HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = tempPassword
+
 	regErr := UserInterface.Register(user)
 	return regErr
 }
@@ -21,15 +30,39 @@ func DeleteProfile(email string) error {
 }
 
 func UpdateProfile(user *models.User) error {
+	tempPassword, err := HashPassword(user.Password)
+	if err != nil {
+		return err
+	}
+	user.Password = tempPassword
+
 	if updateErr := UserInterface.UpdateProfile(user); updateErr != nil {
 		return updateErr
 	}
 	return nil
 }
 
-func CheckPassword(logPass, userPass string) error {
-	if logPass == userPass {
-		return nil
+func CheckPassword(loginPass, hashedPass string) error {
+	if err := bcrypt.CompareHashAndPassword([]byte(hashedPass), []byte(loginPass)); err != nil {
+		return errors.New("wrong credential(password)")
 	}
-	return errors.New("wrong credential(password)")
+	return nil
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func RemoveSensitiveData(users []models.User) []types.CustomResponse {
+	var finalUsers []types.CustomResponse
+	for _, val := range users {
+		finalUsers = append(finalUsers, types.CustomResponse{
+			ID:        val.ID,
+			Username:  val.Username,
+			Email:     val.Email,
+			UpdatedAt: val.UpdatedAt,
+		})
+	}
+	return finalUsers
 }
